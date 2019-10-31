e8191bb38e50b1303b4e2f6c5811bad85f8f6080
ssnhrytsiv <nazar.hrytsiv7@gmail.com>
2019-10-31 05:15:32 +0300

    added to init important import and create class for user

--- user_service/__init__.py
+++ user_service/__init__.py
@@ -1,5 +1,12 @@
 from flask import Flask
+from flask_restful import Api
+from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+api = Api(app)
+ma = Marshmallow(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-# api = restful.Api(app)
--- user_service/models/user.py
+++ user_service/models/user.py
@@ -1,1 +1,24 @@
 """Model for user-service"""
+from user_service import db
+from flask_security import UserMixin
+
+
+class User(db.Model, UserMixin):
+    """ Represent database table user by class
+    :param: integer id for user
+    :param: string name for user
+    :param: string email for user
+    """
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(15), nullable=False, unique=True)
+    user_email = db.Column(db.String(15), nullable=False, unique=True) #EmailType
+    user_password = db.Column(db.String(100), nullable=False)
+    registration_data = db.Column(db.DateTime())
+    # first name and last name?
+
+    def __init__(self, user_name, user_email, user_password):
+        self.user_name = user_name
+        self.email = user_email
+        self.user_password = user_password
