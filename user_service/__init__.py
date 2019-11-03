6eef12b25c20cafeae4cd590b9e43d01f1695795
ssnhrytsiv <nazar.hrytsiv7@gmail.com>
2019-11-04 00:10:19 +0300

    added the jwt manager

--- user_service/__init__.py
+++ user_service/__init__.py
@@ -6,18 +6,20 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate,MigrateCommand
 from flask_script import Manager
 from flask_bcrypt import Bcrypt
-
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 api = Api(app)
-marshmallow = Marshmallow(app)
+
 bcrypt = Bcrypt(app)
 
+jwt = JWTManager(app)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres+psycopg2://postgres:snoopy1@127.0.0.1:5432/UserDB'
-
+app.config['SECRET_KEY'] = 'jwt-secret-string'
 
 db = SQLAlchemy(app)
+marshmallow = Marshmallow(app)
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
