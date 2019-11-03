cce1d5f8c431981369e67d30d9e8d2000c68af06
ssnhrytsiv <nazar.hrytsiv7@gmail.com>
2019-11-03 17:39:53 +0300

    changed inheritance for schema

--- user_service/serializers/user_schema.py
+++ user_service/serializers/user_schema.py
@@ -3,12 +3,13 @@ from user_service import marshmallow
 from marshmallow import fields
 
 
-class UserSchema(marshmallow.ModelSchema):
-    user_id = fields.Int(dump_only=True)
+class UserSchema(marshmallow.Schema):
+    id = fields.Integer(dump_only=True)
     user_name = fields.Str()
-    user_email = fields.Email()
+    user_email = fields.Str()
     user_password = fields.Str()
     user_first_name = fields.Str()
     user_last_name = fields.Str()
-    user_image_file = fields.Url()
+    user_image_file = fields.Str()
     user_registration_data = fields.DateTime(dump_only=True)
+
