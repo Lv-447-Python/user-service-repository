c3131be995e7473f1dfc03ee08ca75a15e65e7a5
ssnhrytsiv <nazar.hrytsiv7@gmail.com>
2019-11-03 03:35:28 +0300

    modified user schema with field

--- user_service/serializers/user_schema.py
+++ user_service/serializers/user_schema.py
@@ -1,8 +1,15 @@
 """Schema for user-service"""
-from user_service import ma
+from user_service import marshmallow
 from user_service.models.user import User
+from marshmallow import Schema, fields
 
 
-class UserSchema(ma.ModelSchema):
-    class Meta:
-        model = User
+class UserSchema(marshmallow.ModelSchema):
+    user_id = fields.Int(dump_only=True)
+    user_name = fields.Str()
+    user_email = fields.Email()
+    user_password = fields.Str()
+    user_first_name = fields.Str()
+    user_last_name = fields.Str()
+    user_image_file = fields.Url()
+    user_registration_data = fields.DateTime(dump_only=True)
