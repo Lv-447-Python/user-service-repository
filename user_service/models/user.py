8b941815bdad5ac4edb5c6630394c2c0ed97464b
ssnhrytsiv <nazar.hrytsiv7@gmail.com>
2019-11-09 23:35:36 +0300

    removed from user model methods and added to utils this methods

--- user_service/models/user.py
+++ user_service/models/user.py
@@ -27,26 +27,7 @@ class User(db.Model, UserMixin):
     user_image_file = db.Column(db.String(25), nullable=False)
     user_registration_data = db.Column(db.DateTime(), nullable=False,
                                        default=datetime.datetime.now())
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-#kwargs
-    @classmethod
-    def find_by_user_name(cls,user_name):
-        return cls.query.filter_by(user_name=user_name).first()
+    
     @classmethod
     def find_user(cls,**kwargs):
-        return cls.query.filter_by(kwargs=kwargs).first()
-#go to utils
-    def get_reset_token(self, expires_sec=1800):
-        s = Serializer(app.config['SECRET_KEY'], expires_sec)
-        return s.dumps({'user_name': self.user_name}).decode('utf-8')
-#go to utils
-    @staticmethod
-    def verify_reset_token(token):
-        s = Serializer(app.config['SECRET_KEY'])
-        try:
-            user_name = s.loads(token)['user_name']
-        except:
-            return None
-        return User.query.filter_by(user_name=user_name).first()
+        return cls.query.filter_by(**kwargs).first()
