b47ba4a059e9299f406ab01402df5d3e725814b2
ssnhrytsiv <nazar.hrytsiv7@gmail.com>
2019-11-15 03:01:53 +0300

    LVPYTT-145 refactoring code with pylint

--- user_service/serializers/user_schema.py
+++ user_service/serializers/user_schema.py
@@ -1,14 +1,23 @@
 """Schema for user-service"""
-from user_service import marshmallow
 from marshmallow import fields
+from user_service import MARSHMALLOW
 from user_service.models.user import User
 
 
-class UserSchema(marshmallow.ModelSchema):
+class UserSchema(MARSHMALLOW.ModelSchema):
+    """
+    Schema for User Model
+    """
     class Meta:
+        """
+        Class meta for model schema by user
+        """
         model = User
 
 
-class LoginSchema(marshmallow.Schema):
+class LoginSchema(MARSHMALLOW.Schema):
+    """
+    Schema for Auth functionality
+    """
     user_name = fields.Str()
     user_password = fields.Str()
--- user_service/utils/user_utils.py
+++ user_service/utils/user_utils.py
@@ -1,17 +1,35 @@
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from user_service import app
+"""Utils for user service"""
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer, SignatureExpired
+from user_service import APP
 from user_service.models.user import User
 
 
-def get_reset_token(object, expires_sec=1800):
-    s = Serializer(app.config['SECRET_KEY'], expires_sec)
-    return s.dumps({'user_name': object.user_name}).decode('utf-8')
+def get_reset_token(user, expires_sec=1800):
+    """
+    Create a specify token for reset user password
+    Args:
+        user:
+        expires_sec:
+
+    Returns:
+        token: string
+    """
+    hash_token_password = Serializer(APP.config['SECRET_KEY'], expires_sec)
+    return hash_token_password.dumps({'user_name': user.user_name}).decode('utf-8')
 
 
 def verify_reset_token(token):
-    s = Serializer(app.config['SECRET_KEY'])
+    """
+    Find user by token
+    Args:
+        token:
+
+    Returns:
+    instance of User or None
+    """
+    hash_token_password = Serializer(APP.config['SECRET_KEY'])
     try:
-        user_name = s.loads(token)['user_name']
-    except:
+        user_name = hash_token_password.loads(token)['user_name']
+    except SignatureExpired:
         return None
     return User.query.filter_by(user_name=user_name).first()
